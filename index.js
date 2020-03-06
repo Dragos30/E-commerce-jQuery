ab3c82773a67dcd99fa8cc701728e6a9f6326221
Dragos30 <49732388+Dragos30@users.noreply.github.com>
2020-03-06 10:43:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,508 @@
+
+         
+            $(document).ready(function(){
+              $("#up-arrow").click(function(){
+              $("#card1").slideToggle(1000);
+              });
+            });
+            $(document).ready(function(){
+            $("#up-arrow2").click(function(){
+              $("#card2").slideToggle(1000);
+              });
+            });
+            $(document).ready(function(){
+              $("#up-arrow3").click(function(){
+                $("#card3").slideToggle(1000);
+                });
+           
+          $(document).ready(function(){
+         $("#up-arrow4").click(function(){
+         $("#card4").slideToggle(1000);
+         });
+          });      
+          
+          $(document).ready(function(){
+            $("#up-arrow5").click(function(){
+            $("#card5").slideToggle(1000);
+            });
+             });    
+             $(document).ready(function(){
+              $("#up-arrow6").click(function(){
+              $("#card6").slideToggle(1000);
+              });
+               });    
+              
+               $(document).ready(function(){
+                $("#up-arrow7").click(function(){
+                $("#card7").slideToggle(1000);
+                });
+                 });    
+             
+                 $(document).ready(function(){
+                  $("#up-arrow8").click(function(){
+                  $("#card8").slideToggle(1000);
+                  });
+                   });    
+                    
+                      
+               $(document).ready(function(){
+                $("#up-arrow9").click(function(){
+                $("#card9").slideToggle(1000);
+                });
+                 });    
+                   
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow10").click(function(){
+                $("#card10").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow11").click(function(){
+                $("#card11").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow12").click(function(){
+                $("#card12").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow13").click(function(){
+                $("#card13").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow14").click(function(){
+                $("#card14").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow15").click(function(){
+                $("#card15").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow16").click(function(){
+                $("#card16").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow17").click(function(){
+                $("#card17").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow18").click(function(){
+                $("#card18").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow19").click(function(){
+                $("#card19").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow20").click(function(){
+                $("#card20").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow21").click(function(){
+                $("#card21").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow22").click(function(){
+                $("#card22").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow23").click(function(){
+                $("#card23").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow24").click(function(){
+                $("#card24").slideToggle(1000);
+                });
+                 });    
+                
+
+                  
+               $(document).ready(function(){
+                $("#up-arrow25").click(function(){
+                $("#card25").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow26").click(function(){
+                $("#card26").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow27").click(function(){
+                $("#card27").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow28").click(function(){
+                $("#card28").slideToggle(1000);
+                });
+                 });    
+                
+                  
+               $(document).ready(function(){
+                $("#up-arrow29").click(function(){
+                $("#card29").slideToggle(1000);
+                });
+                 });    
+
+                 $(document).ready(function(){
+                  $("#up-arrow30").click(function(){
+                  $("#card30").slideToggle(1000);
+                  });
+                   });    
+
+                   $(document).ready(function(){
+                    $("#up-arrow31").click(function(){
+                    $("#card31").slideToggle(1000);
+                    });
+                     });    
+                
+                     $(document).ready(function(){
+                      $("#up-arrow32").click(function(){
+                      $("#card32").slideToggle(1000);
+                      });
+                       });    
+
+
+           $(document).ready(function(){
+            $(".menu-toggle").dropdown(); 
+           $("#menu").toggle(1000);
+           $("#menu1").toggle(1500);
+           $("#menu2").toggle(2000);
+           $("#menu3").toggle(2500);
+           $("#menu4").toggle(3000);
+           });
+          });
+
+          $(document).ready(function(){
+            $("#addItem1").click(function(){
+              $(".item_add:eq(0)").hide();
+              $(".cardText:eq(0)").clone(true).appendTo("#productList");
+              $(".cardPrice:eq(0)").clone(true).appendTo("#priceList");
+            });
+          });
+
+          $(document).ready(function(){
+            $("#addItem2").click(function(){
+              $(".item_add:eq(1)").hide();
+              $(".cardText:eq(1)").clone(true).appendTo("#productList");
+              $(".cardPrice:eq(1)").clone(true).appendTo("#priceList");
+            });
+          });
+
+          $(document).ready(function(){
+            $("#addItem3").click(function(){
+              $(".item_add:eq(2)").hide();
+              $(".cardText:eq(2)").clone(true).appendTo("#productList");
+              $(".cardPrice:eq(2)").clone(true).appendTo("#priceList");
+            });
+          });
+          $(document).ready(function(){
+            $("#addItem4").click(function(){
+              $(".item_add:eq(3)").hide();
+              $(".cardText:eq(3)").clone(true).appendTo("#productList");
+              $(".cardPrice:eq(3)").clone(true).appendTo("#priceList");
+            });
+          });
+
+          $(document).ready(function(){
+            $("#addItem5").click(function(){
+              $(".item_add:eq(4)").hide();
+              $(".cardText:eq(4)").clone(true).appendTo("#productList");
+              $(".cardPrice:eq(4)").clone(true).appendTo("#priceList");
+            });
+          });
+
+        $(document).ready(function(){
+          $("#addItem6").click(function(){
+            $(".item_add:eq(5)").hide();
+            $(".cardText:eq(5)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(5)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem7").click(function(){
+            $(".item_add:eq(6)").hide();
+            $(".cardText:eq(6)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(6)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem8").click(function(){
+            $(".item_add:eq(7)").hide();
+            $(".cardText:eq(7)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(7)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem9").click(function(){
+            $(".item_add:eq(8)").hide();
+            $(".cardText:eq(8)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(8)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem10").click(function(){
+            $(".item_add:eq(9)").hide();
+            $(".cardText:eq(9)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(9)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem11").click(function(){
+            $(".item_add:eq(10)").hide();
+            $(".cardText:eq(10)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(10)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem12").click(function(){
+            $(".item_add:eq(11)").hide();
+            $(".cardText:eq(11)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(11)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem13").click(function(){
+            $(".item_add:eq(12)").hide();
+            $(".cardText:eq(12)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(12)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem14").click(function(){
+            $(".item_add:eq(13)").hide();
+            $(".cardText:eq(13)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(13)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem15").click(function(){
+            $(".item_add:eq(14)").hide();
+            $(".cardText:eq(14)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(14)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem16").click(function(){
+            $(".item_add:eq(15)").hide();
+            $(".cardText:eq(15)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(15)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem17").click(function(){
+            $(".item_add:eq(16)").hide();
+            $(".cardText:eq(16)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(16)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem18").click(function(){
+            $(".item_add:eq(17)").hide();
+            $(".cardText:eq(17)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(17)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem19").click(function(){
+            $(".item_add:eq(18)").hide();
+            $(".cardPrice:eq(18)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(18)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem20").click(function(){
+            $(".item_add:eq(19)").hide();
+            $(".cardText:eq(19)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(19)").clone(true).append("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem21").click(function(){
+            $(".item_add:eq(20)").hide();
+            $(".cardText:eq(20)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(20)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem22").click(function(){
+            $(".item_add:eq(21)").hide();
+            $(".cardText:eq(21)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(21)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem23").click(function(){
+            $(".item_add:eq(22)").hide();
+            $(".cardText:eq(22)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(22)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem24").click(function(){
+            $(".item_add:eq(23)").hide();
+            $(".cardText:eq(23)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(23)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem25").click(function(){
+            $(".item_add:eq(24)").hide();
+            $(".cardText:eq(24)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(24)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem26").click(function(){
+            $(".item_add:eq(25)").hide();
+            $(".cardText:eq(25)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(25)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem27").click(function(){
+            $(".item_add:eq(26)").hide();
+            $(".cardText:eq(26)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(26)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem28").click(function(){
+            $(".item_add:eq(27)").hide();
+            $(".cardText:eq(27)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(27)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem29").click(function(){
+            $(".item_add:eq(28)").hide();
+            $(".cardText:eq(28)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(28)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem30").click(function(){
+            $(".item_add:eq(29)").hide();
+            $(".cardText:eq(29)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(29)").clone(true).appendTo("#priceList");
+          });
+        });
+
+        $(document).ready(function(){
+          $("#addItem31").click(function(){
+            $(".item_add:eq(30)").hide();
+            $(".cardText:eq(30)").clone(true).appendTo("#productList");
+            $(".cardPrice:eq(30)").clone(true).appendTo("#priceList");
+          });
+        });
+            
+           
+            $(document).ready(function(){
+              $("#calc").click(function(){ 
+              var sum = 0;
+              $('#priceList .cardPrice').each(function(){
+               
+                sum += parseFloat($(this).text());
+                
+            });
+              $("#total").text("Your total amount is:"+sum+"$");
+              });
+            });
+       
+   
+
+        $(document).ready(function(){
+        var itemCount = 0;
+        $('.item_add').click(function (){
+          itemCount ++;
+          $('#itemCount').html(itemCount).css('display', 'block');
+        }); 
+            
+         $('.clear').click(function() {
+          itemCount = 0;
+          $(".item_add").show();
+          $('#itemCount').html('').css('display', 'none'); 
+          $('#priceList .cardPrice').remove();
+          $('#productList .cardText').remove();
+          $('#total').sum=0;
+         });
+      });
+
+         $(document).ready(function(){
+         $("#complete").click(function(){
+         $("#part5").slideToggle(2000);
+         });
+         });
